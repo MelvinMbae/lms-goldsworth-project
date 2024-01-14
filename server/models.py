0ee6ddeb36a1169055c044783263947e396a9966
MelvinMbae <melvinmuthomi95@gmail.com>
2024-01-14 20:08:27 +0300

    students model and password authentication

--- server/models.py
+++ server/models.py
@@ -77,3 +77,31 @@ class Teacher(db.Model, SerializerMixin):
     def authenticate(self, password):
         return bcrypt.check_password_hash(
             self._password_hash, password.encode('utf-8'))
+
+class Student(db.Model, SerializerMixin):
+    __tablename__='students'
+    
+    id=db.Column(db.Integer, primary_key=True)
+    first_name=db.Column(db.String)
+    last_name=db.Column(db.String)
+    email=db.Column(db.String, unique=True)
+    _password_hash = db.Column(db.String, nullable=False)
+    created_at=db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at=db.Column(db.DateTime, onupdate=datetime.utcnow)
+    parent_id=db.Column(db.Integer, db.ForeignKey('parents.id'))
+    
+    courses = db.relationship('Course', secondary=course_participant, backref='students')
+    
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+        
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
