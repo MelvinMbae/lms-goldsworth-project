a043947f985a1a8325edd715ac520f64d7f314ae
MelvinMbae <melvinmuthomi95@gmail.com>
2024-01-14 20:06:23 +0300

    teachers model and join table for many to many relationship with courses

--- server/models.py
+++ server/models.py
@@ -37,3 +37,43 @@ class Parent(db.Model, SerializerMixin):
     def authenticate(self, password):
         return bcrypt.check_password_hash(
             self._password_hash, password.encode('utf-8'))
+        
+#join table
+course_participant = db.Table(
+    'course_participant',
+    db.Column('teacher_id', db.Integer, db.ForeignKey('teachers.id')),
+    db.Column('student_id', db.Integer, db.ForeignKey('students.id')),
+    db.Column('course_id', db.Integer, db.ForeignKey('courses.id')),
+    db.PrimaryKeyConstraint('teacher_id','student_id','course_id')
+)
+
+class Teacher(db.Model, SerializerMixin):
+    __tablename__='teachers'
+    
+    id=db.Column(db.Integer, primary_key=True)
+    first_name=db.Column(db.String)
+    last_name=db.Column(db.String)
+    email=db.Column(db.String, unique=True)
+    _password_hash = db.Column(db.String, nullable=False)
+
+    expertise=db.Column(db.String)
+    department=db.Column(db.String)
+    created_at=db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at=db.Column(db.DateTime, onupdate=datetime.utcnow)
+    
+    courses = db.relationship('Course', secondary=course_participant, backref='teachers')
+    
+    # password authentication
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+        
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
