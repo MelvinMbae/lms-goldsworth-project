16cbf80ee616bd6a61906a910518356c223fbd5a
Laleti Cynthia <+>
2024-01-15 19:11:02 +0300

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Login from './Login';
+import './App.css'
 
 function App() {
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+
---
+++ client/src/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+      Login cards for teacher, student and parent
+    </div>
+  )
+}
+
+export default Login
