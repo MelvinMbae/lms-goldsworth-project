9170db177d74d9a820bbb4876b5ebfb2262c628c
MelvinMbae <melvinmuthomi95@gmail.com>
2024-01-15 11:33:03 +0300

    schema updated and join tables adjusted

--- server/models.py
+++ server/models.py
@@ -4,7 +4,6 @@ from flask_bcrypt import Bcrypt
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
 from datetime import datetime
-# from app import app
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -16,7 +15,7 @@ class Parent(db.Model, SerializerMixin):
     first_name=db.Column(db.String, nullable=False)
     last_name=db.Column(db.String, nullable=False)
     email=db.Column(db.String, unique=True, nullable=False)
-    _password_hash = db.Column(db.String, nullable=False)
+    _password = db.Column(db.String, nullable=False)
     imageUrl= db.Column(db.NVARCHAR)
     created_at=db.Column(db.DateTime, default=datetime.utcnow)
     updated_at=db.Column(db.DateTime, onupdate=datetime.utcnow)
@@ -25,30 +24,36 @@ class Parent(db.Model, SerializerMixin):
     
     # password authentication
     @hybrid_property
-    def password_hash(self):
-        return self._password_hash
+    def password(self):
+        return self._password
     
-    @password_hash.setter
-    def password_hash(self, password):
+    @password.setter
+    def password(self, password):
         password_hash = bcrypt.generate_password_hash(
             password.encode('utf-8'))
-        self._password_hash = password_hash.decode('utf-8')
+        self._password = password_hash.decode('utf-8')
         
     def authenticate(self, password):
         return bcrypt.check_password_hash(
-            self._password_hash, password.encode('utf-8'))
+            self._password, password.encode('utf-8'))
         
     def __repr__(self):
         return f"{self.first_name}{self.last_name} is a parent signed up on our app"
         
         
 #join table
-course_participant = db.Table(
-    'course_participant',
-    db.Column('teacher_id', db.Integer, db.ForeignKey('teachers.id')),
-    db.Column('student_id', db.Integer, db.ForeignKey('students.id')),
-    db.Column('course_id', db.Integer, db.ForeignKey('courses.id')),
-    db.PrimaryKeyConstraint('teacher_id','student_id','course_id')
+course_teacher = db.Table(
+    'course_teacher',
+    db.Column('teacher_id', db.Integer, db.ForeignKey('teachers.id'), primary_key=True),
+    db.Column('course_id', db.Integer, db.ForeignKey('courses.id'), primary_key=True)
+    
+)
+
+course_student = db.Table(
+    'course_student',
+    db.Column('student_id', db.Integer, db.ForeignKey('students.id'), primary_key=True),
+    db.Column('course_id', db.Integer, db.ForeignKey('courses.id'), primary_key=True)
+    
 )
 
 class Teacher(db.Model, SerializerMixin):
@@ -58,29 +63,29 @@ class Teacher(db.Model, SerializerMixin):
     first_name=db.Column(db.String, nullable=False)
     last_name=db.Column(db.String, nullable=False)
     email=db.Column(db.String, unique=True, nullable=False)
-    _password_hash = db.Column(db.String, nullable=False)
+    _password = db.Column(db.String, nullable=False)
     imageUrl= db.Column(db.NVARCHAR)
     expertise=db.Column(db.String, nullable=False)
     department=db.Column(db.String, nullable=False)
     created_at=db.Column(db.DateTime, default=datetime.utcnow)
     updated_at=db.Column(db.DateTime, onupdate=datetime.utcnow)
     
-    courses = db.relationship('Course', secondary=course_participant, backref='teachers')
+    courses = db.relationship('Course', secondary=course_teacher,backref="teachers")
     
     # password authentication
     @hybrid_property
-    def password_hash(self):
-        return self._password_hash
+    def password(self):
+        return self._password
     
-    @password_hash.setter
-    def password_hash(self, password):
+    @password.setter
+    def password(self, password):
         password_hash = bcrypt.generate_password_hash(
             password.encode('utf-8'))
-        self._password_hash = password_hash.decode('utf-8')
+        self._password = password_hash.decode('utf-8')
         
     def authenticate(self, password):
         return bcrypt.check_password_hash(
-            self._password_hash, password.encode('utf-8'))
+            self._password, password.encode('utf-8'))
         
     def __repr__(self):
         return f"{self.first_name}{self.last_name} is a teacher signed up on our app"
@@ -92,27 +97,27 @@ class Student(db.Model, SerializerMixin):
     first_name=db.Column(db.String, nullable=False)
     last_name=db.Column(db.String, nullable=False)
     email=db.Column(db.String, unique=True, nullable=False)
-    _password_hash = db.Column(db.String, nullable=False)
+    _password = db.Column(db.String, nullable=False)
     imageUrl= db.Column(db.NVARCHAR)
     created_at=db.Column(db.DateTime, default=datetime.utcnow)
     updated_at=db.Column(db.DateTime, onupdate=datetime.utcnow)
     parent_id=db.Column(db.Integer, db.ForeignKey('parents.id'))
     
-    courses = db.relationship('Course', secondary=course_participant, backref='students')
+    courses = db.relationship('Course', secondary=course_student, backref="students")
     
     @hybrid_property
-    def password_hash(self):
-        return self._password_hash
+    def password(self):
+        return self._password
     
-    @password_hash.setter
-    def password_hash(self, password):
+    @password.setter
+    def password(self, password):
         password_hash = bcrypt.generate_password_hash(
             password.encode('utf-8'))
-        self._password_hash = password_hash.decode('utf-8')
+        self._password = password_hash.decode('utf-8')
         
     def authenticate(self, password):
         return bcrypt.check_password_hash(
-            self._password_hash, password.encode('utf-8'))
+            self._password, password.encode('utf-8'))
         
     def __repr__(self):
         return f"{self.first_name}{self.last_name} is a student signed up on our app"
@@ -122,12 +127,9 @@ class Course(db.Model, SerializerMixin):
     
     id=db.Column(db.Integer, primary_key=True)
     course_name=db.Column(db.String, unique=True, nullable=False)
-    description=db.Column(db.String, unique=True, nullable=False)
+    description=db.Column(db.String, nullable=False)
     created_at=db.Column(db.DateTime, default=datetime.utcnow)
     updated_at=db.Column(db.DateTime, onupdate=datetime.utcnow)
-    
-    teachers=db.relationship('Teacher', secondary=course_participant,backref='courses')
-    students=db.relationship('Student', secondary=course_participant,backref='courses')
     
     def __repr__(self):
         return f"{self.course_name}is a course offered in our platform"
@@ -136,9 +138,9 @@ class Content(db.Model, SerializerMixin):
     __tablename__='contents'
     
     id=db.Column(db.Integer, primary_key=True)
-    content_name=db.Column(db.String, unique=True, nullable=False)
-    description=db.Column(db.String, unique=True, nullable=False)
-    content_type=db.Column(db.String, unique=True, nullable=False)
+    content_name=db.Column(db.String)
+    description=db.Column(db.String)
+    content_type=db.Column(db.String, nullable=False)
     created_at=db.Column(db.DateTime, default=datetime.utcnow)
     updated_at=db.Column(db.DateTime, onupdate=datetime.utcnow)
     course_id=db.Column(db.Integer, db.ForeignKey('courses.id'))
---
+++ server/seed.py
@@ -0,0 +1,106 @@
+from random import choice, randint, random
+import json 
+from sqlalchemy.orm.session import make_transient
+
+from faker import Faker
+from app import app
+from models import db,bcrypt, Parent, Teacher, Student,Course,Content
+
+fake = Faker()
+
+with open("C:/Users/Melvin Mbae/Development/Code/phase-5/phase5-project/goldworth-lms/client/db.json", mode="r") as itemdata:
+    data = json.load(itemdata)
+
+item_data = [item for item in data]
+
+with app.app_context():
+
+    Parent.query.delete()
+    Teacher.query.delete()
+    Student.query.delete()
+    
+    Course.query.delete()
+    
+    parents=[]
+    for _ in range(10):
+        parent = Parent(
+            first_name=fake.first_name(),
+            last_name=fake.last_name(),
+            email=fake.email(),
+            password=bcrypt.generate_password_hash(fake.password()).decode('utf-8')
+        )
+        
+        parents.append(parent)
+        
+    db.session.add_all(parents)
+    db.session.commit()
+    
+    teachers=[]
+    for _ in range(10):
+        teacher = Teacher(
+            first_name=fake.first_name(),
+            last_name=fake.last_name(),
+            email=fake.email(),
+            password=bcrypt.generate_password_hash(fake.password()).decode('utf-8'),
+            expertise=fake.word(),
+            department=fake.word()
+        )
+        teachers.append(teacher)
+        
+    db.session.add_all(teachers)
+    db.session.commit()
+    
+    students = []
+    for _ in range(30):
+        student = Student(
+            first_name=fake.first_name(),
+            last_name=fake.last_name(),
+            email=fake.email(),
+            password=bcrypt.generate_password_hash(fake.password()).decode('utf-8'),
+            parent_id=choice(parents).id
+        )
+        students.append(student)
+
+    db.session.add_all(students)
+    db.session.commit()
+    
+  
+    
+    courses=[]
+    for c in data["courses"]:
+        course = Course(
+            course_name= c["course_name"],
+            description=c["description"],
+            )
+        
+        courses.append(course)
+        
+    # print(items)
+    db.session.add_all(courses)
+    db.session.commit()
+    
+    contents=[]
+    for c in data["contents"]:
+        content = Content(
+            content_name= c["content_name"],
+            description=c["description"],
+            content_type=c["content_type"],
+            course_id=c["course_id"])
+        contents.append(content)
+        
+    db.session.add_all(contents)
+    db.session.commit()
+    
+    for teacher in teachers:
+        courses_for_teacher = [choice(courses) for _ in range(randint(1, 3))]
+        for course in courses_for_teacher:
+            teacher.courses.append(course)
+
+    # Associate students with courses and parents
+    for student in students:
+        courses_for_student = [choice(courses) for _ in range(randint(1, 3))]
+        for course in courses_for_student:
+            student.courses.append(course)
+
+    db.session.commit()
+    
