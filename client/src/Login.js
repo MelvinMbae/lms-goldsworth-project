a6e47b68ace466391e7247435cb69a20adf78d11
Laleti Cynthia <+>
2024-01-16 12:01:07 +0300

    login-structure

--- client/src/Login.js
+++ client/src/Login.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>
-      Login cards for teacher, student and parent
+    <div className='login-container'>
+      <h1 id='login-header'>Welcome to Goldworth</h1>
+      <form className='login'>
+        <label>Email Adress<input type='email' /></label>
+        <label>Password<input type='password' /></label>
+      </form>
+      <button className='login-btn'>Login</button>
+
     </div>
   )
 }
