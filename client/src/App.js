09f53967092117f7858746c1d4b1e2d6f7003be6
Laleti Cynthia <+>
2024-01-17 21:50:35 +0300

    changes to main

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,17 @@
-import './Dashboard.css';
-import StudentDash from './StudentDash';
-import TeacherDash from './TeacherDash';
-import ParentDash from './ParentDash';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
 import './App.css'
-import courses from './courses';
+import Courses from './Courses';
 
 function App() {
   return (
-    // <TeacherDash/>
-    // <ParentDash/>
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/dashboard' element={<StudentDash/>} />
-        <Route path='/courses' element={<courses />} />
+        <Route path='/courses' element={<Courses />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/Courses.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+function Courses() {
+ /* const courses = [
+    { title: 'Machine Learning', content: 'Machine learning is a subfield of artificial intelligence, which is broadly defined as the capability of a machine to imitate intelligent human behavior.', image:'https://images.unsplash.com/photo-1523961131990-5ea7c61b2107?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'},
+    { title: 'Web Development', content: 'Web development involves building and maintaining websites. It includes various aspects such as database management, front-end and back-end development.',image:'https://images.unsplash.com/photo-1511376777868-611b54f68947?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+    { title: 'Artificial Intelligence', content: 'Artificial Intelligence (AI) is the simulation of human intelligence in machines. It involves the creation of algorithms that can perform tasks that typically require human intelligence.',image:'https://images.unsplash.com/photo-1677442135703-1787eea5ce01?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+    { title: 'Game Development', content: 'Game development is the process of creating video games. It involves design, programming, testing, and production of games for various platforms.',image:'https://images.unsplash.com/photo-1526925539332-aa3b66e35444?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
+  ];*/
+
+  return (
+    <div className='cards-container'>
+      <div className='course-cards'>
+        <div className='card-info'>
+        <img className='card-image' src='https://images.unsplash.com/photo-1523961131990-5ea7c61b2107?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
+          <h3 id='course-header'>Machine Learning </h3>
+          <p>Machine learning is a subfield of artificial intelligence, which is broadly defined as the capability of a machine to imitate intelligent human behavior.</p>
+        </div>
+         
+      </div>
+      <div className='course-cards'>
+        <div className='card-info'>
+        <img className='card-image' src='https://images.unsplash.com/photo-1523961131990-5ea7c61b2107?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
+          <h3 id='course-header'>Machine Learning </h3>
+          <p>Machine learning is a subfield of artificial intelligence, which is broadly defined as the capability of a machine to imitate intelligent human behavior.</p>
+        </div>
+      </div>
+      <div className='course-cards'>
+        <div className='card-info'>
+        <img className='card-image' src='https://images.unsplash.com/photo-1523961131990-5ea7c61b2107?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
+          <h3 id='course-header'>Machine Learning </h3>
+          <p>Machine learning is a subfield of artificial intelligence, which is broadly defined as the capability of a machine to imitate intelligent human behavior.</p>
+        </div>
+      </div>
+      <div className='course-cards'>
+        <div className='card-info'>
+        <img className='card-image' src='https://images.unsplash.com/photo-1523961131990-5ea7c61b2107?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
+          <h3 id='course-header'>Machine Learning </h3>
+          <p>Machine learning is a subfield of artificial intelligence, which is broadly defined as the capability of a machine to imitate intelligent human behavior.</p>
+        </div>
+      </div>
+    </div>
+      
+  )
+}
+
+export default Courses;
