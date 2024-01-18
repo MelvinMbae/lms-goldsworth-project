24f04ae7c7c60ae6e8c2752c791b52d8489e25c7
IanKamau <theurikamau7@gmail.com>
2024-01-18 20:12:52 +0300

    ian chat

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './Home';
-import Login from './Login';
-import StudentDash from './StudentDash';
-import Navbar from './Navbar';
-import ReportCard from './ReportCard';
-import Courses from './Courses';
-import Dashboard from './Dashboard';
-
-
+import './Chat.css';
+import Chat from './pages/Chat';
+import Registration from './pages/Registration';
 function App() {
-  const [courses, setCourse] = useState([])
-  const [user, setUser] = useState("")
-
-
-  useEffect(()=>{
-    fetch("/courses").then((response)=>{
-      if(response.ok){
-        response.json()
-        .then((courses)=>{
-          setCourse(courses)
-        })
-      }
-    })
-  },[])
-
-  useEffect(()=>{
-    fetch("/checksession").then((response)=>{
-      if(response.ok){
-        response.json()
-        .then((sessionMember)=>{
-          setUser(sessionMember)
-        })
-      }
-    })
-  },[])
-
   return (
-      <Routes>
-        <Route path='/' element={<Navbar user={user} setUser={setUser}/>}>
-          <Route index element={<Home courses={courses}/>} />
-          <Route path='/' element={<Home courses={courses}/>} />
-          <Route  element={<Dashboard user={user}/>}>
-            <Route path='/dashboard' element={<StudentDash />} />
-            <Route path='/reportcard' element={<ReportCard />} />
-            <Route path='/courses' element={<Courses />} />
-          </Route>
-        </Route>
-        <Route path='/login' element={<Login setUser={setUser}/>} />
-      </Routes>
+    <div className="app">
+      {<Chat />}
+      {/*<Registration/>*/}
+    </div>
   );
 }
 
 export default App;
-
-
