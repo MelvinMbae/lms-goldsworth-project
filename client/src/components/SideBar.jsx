18deb0d33f2e981ac265e6fe093558120e4dfdb1
Njogu Michael <michaelfebzen.mb@gmail.com>
2024-01-22 20:39:35 +0300

    Added nav

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -3,16 +3,33 @@ import { useLocation } from "react-router-dom";
 
 function SideBar() {
 
+    function openNav() {
+        document.getElementById("mySide-nav").style.width = "250px";
+        document.getElementById("main").style.display='none';
+     }
+     
+     function closeNav() {
+        document.getElementById("mySide-nav").style.width = "0";
+        document.getElementById("main").style.display = "block";
+     }
+
     const location = useLocation();
 
     function Sidebar(props){
         return (
-            <div className='sides'>
+            <div className="assignment-container">
+            <div id="mySide-nav" class="side-nav"> 
+                <a href="javascript:void(0)" class="closebtn" onClick={() => closeNav()}>x</a>
                 <Link to={'/dashboard'}>Dashboard</Link>
-                <Link to={'/active-courses'}>Active Courses</Link>
-                <Link to={'/classes'}>Classes</Link>
-                {props.children}
+                <Link to={'/courses'}>Courses</Link>
+                <Link to={'/discussion'}>Discussion</Link>
+                <Link to={'/assignments'}>Assignments</Link>
+                <Link to={'/calender'}>Calender</Link>
+                <Link to={'/reportcard'}>ReportCard</Link>
             </div>
+            <div id="main">
+                <span style={{fontSize:'30px', cursor:'pointer'}} onClick={() => openNav()}>☰</span>
+            </div></div>
             )
         }
 
@@ -27,3 +44,7 @@ function SideBar() {
   )
 }
 export default SideBar;
+
+
+
+
