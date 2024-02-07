db63b77f4f7b42952321dc83ec3922b5579f02ff
MelvinMbae <melvinmuthomi95@gmail.com>
2024-02-07 19:26:43 +0300

    css

--- client/src/ParentDash.js
+++ client/src/ParentDash.js
@@ -22,7 +22,12 @@ function ParentDash() {
                 "email": "Trevour.Kitavi@student.goldworth.com",
                 "image_url": null,
                 "name": "Trevour Kitavi",
-                "report_card": [{ "id": 1, "topic": "Data Structures", "grade": 80 }, { "id": 2, "topic": "Machine Learning", "grade": 80 }],
+                "report_card": [{ "id": 1, "topic": "Cyber_Security Discuss man before.", "grade": 80 },
+                { "id": 2, "topic": "Machine Learning", "grade": 80 },
+                { "id": 3, "topic": "Topic 2", "grade": 70 },
+                { "id": 4, "topic": "React Native", "grade": 66 },
+                { "id": 5, "topic": "Mobile Dev", "grade": 78 },
+                { "id": 8, "topic": "Machine Learning for dummies", "grade": 99 }],
                 "student_id": 11
             },
             {
@@ -49,13 +54,15 @@ function ParentDash() {
     };
 
     return (
-        <div className="parent-dash-details">
+        <div className="parentdash-contents">
             <div className="parent-section">
                 <h2>Name: {user.name}</h2>
                 <h3>Email: {user.email}</h3>
                 <h3>Parent_ID: PTID_{user.parent_id}</h3>
             </div>
-            <div className="announcement-title"><h3>Announcements</h3></div>
+            <div className="announcement-title">
+                <u><h3>Announcements</h3></u>
+            </div>
 
             <div className="announcement-section">
 
@@ -67,7 +74,7 @@ function ParentDash() {
                 ))}
             </div>
             <div className="announcement-title">
-                <h3>View Students</h3>
+                <u><h3>View Students</h3></u>
             </div>
             <div className="student-dropdown">
                 <select id="student-select" onChange={handleStudentSelect}>
@@ -78,34 +85,35 @@ function ParentDash() {
                 </select>
             </div>
 
+            <div className="viewstudent-section">
+                {selectedStudent && (
+                    <div className='parent-student-data'>
+                        <u><h3>Student Details</h3></u>
+                        <h4>Name: {selectedStudent.name}</h4>
+                        <h4>Email: {selectedStudent.email}</h4>
+                        <h4>Student ID: STD_{selectedStudent.student_id}</h4>
+                    </div>
 
-            {selectedStudent && (
-                <div className='parent-student-data'>
-                    <u><h3>Student Details</h3></u>
-                    <h4>Name: {selectedStudent.name}</h4>
-                    <h4>Email: {selectedStudent.email}</h4>
-                    <h4>Student ID: STD_{selectedStudent.student_id}</h4>
-                </div>
-
-            )}
-            {selectedStudent && (
-                <div className='parent-student-data'>
-                    <u><h3>Courses Enrolled</h3></u>
-                    {selectedStudent.courses.map((course) => (
-                        <p key={course.id}>{course.course_name}</p>
-                    ))}
-                </div>
-            )}
-            {selectedStudent && (
-                <div className='parent-student-data'>
-                    <u><h3>Past Performance</h3></u>
-                    {selectedStudent.report_card.map((report) => (
-                        <div key={report.id}>
-                            <p>{report.topic}: {report.grade}%</p>
-                        </div>
-                    ))}
-                </div>
-            )}
+                )}
+                {selectedStudent && (
+                    <div className='parent-student-data'>
+                        <u><h3>Courses Enrolled</h3></u>
+                        {selectedStudent.courses.map((course) => (
+                            <p key={course.id}>{course.course_name}</p>
+                        ))}
+                    </div>
+                )}
+                {selectedStudent && (
+                    <div className='parent-student-data'>
+                        <u><h3>Past Performance</h3></u>
+                        {selectedStudent.report_card.map((report) => (
+                            <div key={report.id}>
+                                <p>{report.topic}: {report.grade}%</p>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
